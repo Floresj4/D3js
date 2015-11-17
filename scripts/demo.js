3c3e03334c048976b3a39596a773a9560e07a09a
Jason <floresj4@gmail.com>
2015-11-16 23:11:55 -0500

    changing function name x -> linearScale; was confusing

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,14 +1,14 @@
 var data = [4, 8, 15, 16, 23, 42, 1, 9, 66];
 
 function my_code(){
-	var x = d3.scale.linear()
+	var linearScale = d3.scale.linear()
 		.domain([0, d3.max(data)])
-		.range([0, 420]);
+		.range([0, 660]);
 
 	d3.select(".chart")
 		.selectAll("div")
 		.data(data)
 		.enter().append("div")
-		.style("width", function(d) { return x(d) + "px"; })
+		.style("width", function(d) { return linearScale(d) + "px"; })
 		.text(function(d) { return d; });
 }
