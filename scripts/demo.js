5b4b306241a67f05b4b340252bf81f7411c7eb5f
Jason <floresj4@gmail.com>
2015-11-18 00:01:38 -0500

    updating to data load from external file

--- scripts/demo.js
+++ scripts/demo.js
@@ -18,29 +18,43 @@ function svg_build() {
 	var barHeight = 20;
 	
 	var linearScale = d3.scale.linear()
-		.domain([0, d3.max(data)])
 		.range([0, width]);
 	
 	var chart = d3.select(".chart")
-		.attr("width", width)
-		.attr("height", barHeight * data.length);
+		.attr("width", width);
+		
 	
-	var bar = chart.selectAll("g")
-		.data(data)
-		.enter().append("g")
-		.attr("transform", function(d, i) { 
-			return "translate(0, " + i * barHeight + ")"; 
-		});
-	
-	bar.append("rect")
-		.attr("width", linearScale)
-		.attr("height", barHeight - 1);
-	
-	bar.append("text")
-		.attr("x", function(d) {
-			return linearScale(d) - 3;
-		})
-		.attr("y", barHeight / 2)
-		.attr("dy", ".35em")
-		.text(function(d) { return d; });
+	d3.tsv("./scripts/data.tsv", type, function(error, data) {
+		linearScale.domain([0, d3.max(data, function(d) {
+			return d.value;
+		})]);
+		
+		//set the height before display
+		chart.attr("height", barHeight * data.length);
+		
+		var bar = chart.selectAll("g")
+			.data(data)
+			.enter().append("g")
+			.attr("transform", function(d, i) { 
+				return "translate(0, " + i * barHeight + ")"; 
+			});
+
+		bar.append("rect")
+			.attr("width", function(d) { return linearScale(d.value); })
+			.attr("height", barHeight - 1);
+
+		bar.append("text")
+			.attr("x", function(d) {
+				return linearScale(d.value) - 3;
+			})
+			.attr("y", barHeight / 2)
+			.attr("dy", ".35em")
+			.text(function(d) { return d.value; });
+	});
+}
+
+function type(d) {
+	//coerce to number
+	d.value = +d.value;
+	return d;
 }
