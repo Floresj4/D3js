0474d2c32b32479515fa372b7f1e1c6e4b485145
Jason <floresj4@gmail.com>
2015-11-18 23:31:31 -0500

    updating dataset, build vertical

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,19 +1,52 @@
-var data = [4, 8, 15, 16, 23, 42, 1, 9, 66];
+var data = [4, 8, 15, 16, 23, 42, 1, 9, 66, 42, 0, ];
+var margin = {top: 20, right: 30, bottom: 30, left:40};
 
-function html_build(){
-	var linearScale = d3.scale.linear()
-		.domain([0, d3.max(data)])
-		.range([0, 660]);
+function build_vertical() {
+	//account for margins
+	var width = 960 - margin.left - margin.right;
+	var height = 500 - margin.top - margin.bottom;
+	
+	var yscale = d3.scale.linear()
+		.range([height, 0]);
+	
+	var oscale = d3.scale.ordinal()
+		.rangeRoundBands([0, width], .1);
+	
+	//select the chart & set
+	var chart = d3.select(".chart")
+		.attr("width", width)
+		.attr("height", height);
+		
+	//load data
+	d3.tsv("./scripts/data.tsv", type, function(error, data) {
+		
+		oscale.domain(data.map(function(d){ return d.letter; }));
+		
+		yscale.domain([0, d3.max(data, function(d) { return d.value; })]);
+		
+		var barWidth = width / data.length;
+		
+		var bar = chart.selectAll("g")
+			.data(data)
+			.enter().append("g")
+			.attr("transform", function(d, i) {
+				return "translate(" + oscale(d.letter) + ", 0)"; 
+			});
 
-	d3.select(".chart")
-		.selectAll("div")
-		.data(data)
-		.enter().append("div")
-		.style("width", function(d) { return linearScale(d) + "px"; })
-		.text(function(d) { return d; });
+		bar.append("rect")
+			.attr("y", function(d) { return yscale(d.value); })
+			.attr("height", function(d) { return height - yscale(d.value); })
+			.attr("width", oscale.rangeBand());
+
+		bar.append("text")
+			.attr("x", oscale.rangeBand() / 2)
+			.attr("y", function(d) { return yscale(d.value) + 3; })
+			.attr("dy", ".75em")
+			.text(function(d) { return d.value; });
+	});
 }
 
-function svg_build() {
+function build_horizontal() {
 	var width = 420;
 	var barHeight = 20;
 	
@@ -22,8 +55,7 @@ function svg_build() {
 	
 	var chart = d3.select(".chart")
 		.attr("width", width);
-		
-	
+
 	d3.tsv("./scripts/data.tsv", type, function(error, data) {
 		linearScale.domain([0, d3.max(data, function(d) {
 			return d.value;
@@ -57,4 +89,17 @@ function type(d) {
 	//coerce to number
 	d.value = +d.value;
 	return d;
+}
+
+function html_build(){
+	var linearScale = d3.scale.linear()
+		.domain([0, d3.max(data)])
+		.range([0, 660]);
+
+	d3.select(".chart")
+		.selectAll("div")
+		.data(data)
+		.enter().append("div")
+		.style("width", function(d) { return linearScale(d) + "px"; })
+		.text(function(d) { return d; });
 }
